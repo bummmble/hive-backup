68133f1980ffe1d87e0915f6d67be9004267d398
Forrest Collins <Forest.d.collins@gmail.com>
2017-12-12 18:02:02 -0600

    Sets up basic repo

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['airbnb'],
+  rules: {
+    'promise/params-names': 1,
+    'import/no-unresolved': 0,
+    'import/named': 0,
+    'import/default': 0,
+    'import/namespace': 0,
+    'import/no-commonjs': 0,
+    'import/prefer-default-export': 0
+  }
+};
